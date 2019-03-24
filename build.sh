84d0e5ff67ce492d7f1e5af26f30c2d9a957ccb8
dhavalgoti24 <dhavalgoti24@gmail.com>
2019-03-24 10:45:59 +0530

    Initializing

---
+++ build.sh
@@ -0,0 +1,40 @@
+KERNEL_DIR=$PWD
+Anykernel_DIR=$KERNEL_DIR/AnyKernel2/
+DATE=$(date +"%d%m%Y")
+KERNEL_NAME="Acetaminofen-initial_build"
+DEVICE="-kenzo-"
+FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE"
+
+# Cleanup before 
+rm -rf $Anykernel_DIR/*zip
+rm -rf $Anykernel_DIR/Image.gz-dtb
+rm -rf arch/arm64/boot/Image
+rm -rf arch/arm64/boot/dts/qcom/kenzo-msm8956-mtp.dtb
+rm -rf arch/arm64/boot/Image.gz
+rm -rf arch/arm64/boot/Image.gz-dtb
+
+# Export few variables
+export KBUILD_BUILD_USER="Dhaval"
+export KBUILD_BUILD_HOST="ryzen"
+export CROSS_COMPILE=/home/dhacommas/Desktop/cm14.1/n/toolchain/bin/aarch64-linux-android-
+export ARCH="arm64"
+export USE_CCACHE=1
+
+# Finally build it
+make clean && make mrproper
+make kenzo_defconfig
+make menuconfig
+make -j5
+
+# Create the flashable zip
+cp arch/arm64/boot/Image.gz-dtb $Anykernel_DIR
+cd $Anykernel_DIR
+zip -r9 $FINAL_ZIP.zip * -x .git README.md *placeholder
+
+# Cleanup again
+cd ../
+rm -rf $Anykernel_DIR/Image.gz-dtb
+rm -rf arch/arm64/boot/Image
+rm -rf arch/arm64/boot/dts/qcom/kenzo-msm8956-mtp.dtb
+rm -rf arch/arm64/boot/Image.gz
+rm -rf arch/arm64/boot/Image.gz-dtb
